3565ea085a00d25fe3be1de42717f161b24978cb
Edson Sousa <edson3711.es@gmail.com>
2023-12-11 00:18:57 -0300

    Feat: criando undade table model

---
+++ src/tableModel/UnidadeTableModel.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tableModel;
+
+/**
+ *
+ * @author edson
+ */
+import javax.swing.table.AbstractTableModel;
+import java.util.ArrayList;
+import java.util.List;
+import model.Unidade;
+
+public class UnidadeTableModel extends AbstractTableModel {
+
+    private List<Unidade> unidades;
+    private final String[] colunas = {"Codigo", "Sigla", "Descrição", "Fragmentado", "Registro", "Usuario", "Id"};
+
+    public UnidadeTableModel() {
+        this.unidades = new ArrayList<>();
+        // Adicione dados de exemplo se necessário
+    }
+
+    public void setUnidades(List<Unidade> unidades) {
+        this.unidades = unidades;
+        fireTableDataChanged();
+    }
+
+    @Override
+    public int getRowCount() {
+        return unidades.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return colunas.length;
+    }
+
+    @Override
+    public String getColumnName(int columnIndex) {
+        return colunas[columnIndex];
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        Unidade unidade = unidades.get(rowIndex);
+
+        switch (columnIndex) {
+            case 0:
+                return unidade.getId_referencia();
+            case 1:
+                return unidade.getSigla_unidade();
+            case 2:
+                return unidade.getDesc_unidade();
+            case 3:
+                return unidade.getFragmento_unidade();
+            case 4:
+                return unidade.getRegistro_unidade();
+            case 5:
+                return unidade.getUsuario_unidade();
+            case 6:
+                return unidade.getId_unidade();
+            default:
+                return null;
+        }
+    }
+}
