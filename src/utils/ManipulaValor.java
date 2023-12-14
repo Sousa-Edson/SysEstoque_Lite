59da3d685c025208e715fa5283c934bc496995af
Sousa-Edson <edson3711.es@gmail.com>
2023-12-14 11:13:20 -0300

    Feat:ajuste em manipulaValor

--- src/utils/ManipulaValor.java
+++ src/utils/ManipulaValor.java
@@ -10,38 +10,34 @@ package utils;
  */
 import java.math.BigDecimal;
 import java.text.NumberFormat;
-import java.util.Objects;
 
 public class ManipulaValor {
-    
+
     public static void main(String[] args) {
-        System.out.println( ManipulaValor("R$ 34,988"));
+        System.out.println(manipulaValor("R$ 34,96"));
     }
 
-   
+    public static String manipulaValor(String texto) {
+        String meuValor = texto.replace("R$", "").replace(" ", "").replace(".", "").replace(",", ".");
 
-    public  static String  ManipulaValor(String texto) {
-       
-        String MeuValor = texto.replace("R$", "").replace(" ", "").replace(".", "").replace(",", ".");
-
-        if (Objects.equals(MeuValor, null) || MeuValor.trim().isEmpty()) {
-            System.out.println("Interface.ProdCadastroJIF.ManipulaValor()");
+        if (meuValor == null || meuValor.trim().isEmpty()) {
+            System.out.println("manipulaValor: Valor vazio ou nulo");
             return null;
         }
 
         try {
-            double valor = Double.parseDouble(MeuValor);
- 
-                BigDecimal df1 = new BigDecimal(valor);
-                NumberFormat nf1 = NumberFormat.getInstance();
-                nf1.setMinimumFractionDigits(4);
-                nf1.setMaximumFractionDigits(4);
-                String FormatoValorProd = "R$ " + nf1.format(df1); 
-              return FormatoValorProd;
-        } catch (NumberFormatException e) { 
+            double valor = Double.parseDouble(meuValor);
+            BigDecimal decimalValor = BigDecimal.valueOf(valor);
+            NumberFormat numberFormat = NumberFormat.getInstance();
+            numberFormat.setMinimumFractionDigits(4);
+            numberFormat.setMaximumFractionDigits(4);
+            String formatoValorProd = "R$ " + numberFormat.format(decimalValor);
+
+            return formatoValorProd;
+        } catch (NumberFormatException e) {
             System.err.println("Erro ao converter para Double: " + e.getMessage());
-             return null;
+            return null;
         }
-       
     }
+
 }
