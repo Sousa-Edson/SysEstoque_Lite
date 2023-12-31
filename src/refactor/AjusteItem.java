a440bb4c55f3db0f6d6ee28c6bc42c7ea64565ee
Edson Sousa <edson3711.es@gmail.com>
2023-12-31 17:22:19 -0300

    Feat: adicionando classe para ajuste de items

---
+++ src/refactor/AjusteItem.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package refactor;
+
+import ConectaBanco.ConexaoBD;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author edson
+ */
+public class AjusteItem {
+
+    public static void ajusteNotaItem() {
+        ConexaoBD conex = new ConexaoBD();
+        conex.conexao();
+        try {
+            PreparedStatement pstmt = conex.con.prepareStatement(
+                    "UPDATE movprodutobase as m SET nota_mov  = ( SELECT nota.id_nota FROM nota where nota.id_referencianota  = m.nota_mov ORDER BY id_nota DESC LIMIT 1 );");
+
+            pstmt.execute();
+            System.out.println("ajusteNotaItem OK");
+
+        } catch (SQLException ex) {
+            System.out.println("ajusteNotaItem Erro::: " + ex.getMessage());
+
+        } finally {
+            conex.desconecta();
+        }
+    }
+
+    public static void ajusteProdutoItem() {
+        ConexaoBD conex = new ConexaoBD();
+        conex.conexao();
+        try {
+            PreparedStatement pstmt = conex.con.prepareStatement(
+                    " UPDATE movprodutobase as m SET id_prod_ent  = ( SELECT produto.id_prod FROM produto where produto.sis_prod  = m.id_prod_ent ORDER BY produto.id_prod DESC LIMIT 1 );");
+
+            pstmt.execute();
+            System.out.println("ajusteProdutoItem OK");
+
+        } catch (SQLException ex) {
+            System.out.println("ajusteProdutoItem Erro::: " + ex.getMessage());
+
+        } finally {
+            conex.desconecta();
+        }
+    }
+
+    public static void main(String[] args) {
+        ajusteNotaItem();
+    }
+}
