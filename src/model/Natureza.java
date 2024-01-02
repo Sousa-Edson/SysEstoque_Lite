64bd0e87c286b47349d6fed39784471d8ebcdbc7
Edson Sousa <edson3711.es@gmail.com>
2024-01-02 18:57:40 -0300

    Feat: ajustando para exbibir natureza funcionando

--- src/model/Natureza.java
+++ src/model/Natureza.java
@@ -4,6 +4,8 @@
  */
 package model;
 
+import java.util.Objects;
+
 /**
  *
  * @author edson
@@ -73,6 +75,28 @@ public class Natureza {
     @Override
     public String toString() {
         return desc_natureza;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 67 * hash + Objects.hashCode(this.desc_natureza);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Natureza other = (Natureza) obj;
+        return Objects.equals(this.desc_natureza, other.desc_natureza);
     }
 
 }
